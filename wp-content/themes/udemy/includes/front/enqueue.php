b6b93dd92bdb5ea2a8c7c59948cea63d404252b4
deep-tbf <deep@thebrandingfirminc.com>
2023-01-11 12:38:49 -0500

    fonts

--- wp-content/themes/udemy/includes/front/enqueue.php
+++ wp-content/themes/udemy/includes/front/enqueue.php
@@ -3,17 +3,19 @@
  function u_enqueue(){
      wp_register_style(
         'u_font_rubik_pacifico',
-        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap'
+        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+        [],
+        '1.0'
      );
 
      wp_register_style(
         'u_bootstrap_icons',
-        get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css')
+        get_theme_file_uri('/assets/bootstrap-icons/bootstrap-icons.css')
      );
 
      wp_register_style(
         'u_theme',
-        get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css')
+        get_theme_file_uri('/assets/public/index.css')
      );
 
      wp_enqueue_style('u_font_rubik_pacifico');
